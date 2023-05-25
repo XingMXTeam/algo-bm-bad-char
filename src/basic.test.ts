abc5cee4bd19e0f4198f6fdb8879ac194ce84137
Felix Mao <xunxing1989@163.com>
2023-05-25 00:49:09 +0000

    Update basic.test.ts and bm.ts

--- src/basic.test.ts
+++ src/basic.test.ts
@@ -1,5 +1,10 @@
-import   { BMTestFunction } from "./index";
+import   { BM_BC_TestFunction, BMTestFunction } from "./index";
 import { assert, expect, test } from 'vitest'
+
+
+test("BM - bad char - it works", () => {
+  expect(BM_BC_TestFunction()).toBe(5);
+});
 
 
 test("BM - it works", () => {
