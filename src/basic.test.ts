68464d80dc4c062797040968f56975f9163a05b0
felix <813106093@qq.com>
2023-06-18 17:08:10 +0800

    :sparkles: add merge sort algo

--- src/basic.test.ts
+++ src/basic.test.ts
@@ -1,16 +1,22 @@
-import   { BM_BC_TestFunction, BMTestFunction, kmp_TestFunction, trie_TestFunction, rk_TestFunction, ac_TestFunction, hf_TestFunction } from "./index";
-import {  expect, test } from 'vitest'
-
+import {
+  BM_BC_TestFunction,
+  BMTestFunction,
+  kmp_TestFunction,
+  trie_TestFunction,
+  rk_TestFunction,
+  ac_TestFunction,
+  hf_TestFunction,
+    ms_test_function
+} from "./index";
+import { expect, test } from "vitest";
 
 test("文本编辑器的查找与替换： BM - bad char - it works", () => {
   expect(BM_BC_TestFunction()).toBe(5);
 });
 
-
 test("文本编辑器的查找与替换：BM - it works", () => {
   expect(BMTestFunction()).toBe(5);
 });
-
 
 test("文本编辑器的查找与替换: rk - it works", () => {
   expect(rk_TestFunction()).toBe(1);
@@ -28,6 +34,10 @@ test("敏感词过滤：ac - it works", () => {
   expect(ac_TestFunction()).toBe(null);
 });
 
-test("霍夫曼编码：hf - it works", () => {
+test("文件压缩：hf - it works", () => {
   expect(hf_TestFunction()).toBe(null);
+});
+
+test("海量数据处理: merge sort - it works", () => {
+  expect(ms_test_function()).toBe(true);
 });
--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,5 @@ export * from "./文本编辑器的查找与替换/kmp";
 export * from "./搜索关键词提示功能/trie";
 export * from "./文本编辑器的查找与替换/rk";
 export * from "./敏感词过滤/ac";
-export * from "./霍夫曼编码/hf";
+export * from "./文件压缩/hf";
+export * from "./海量数据处理/merge_sort";
